01907e8dd03c7a4ec67a883673720d7d19d69ffb
Rahul <rahulsidhpura912@gmail.com>
2018-06-11 16:21:55 +0530

    10 done as on 11-06-2018

---
+++ os/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,20 @@
+$(function() {
+	
+	switch(menu) {
+	
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	case 'All Products':
+		$('#listProducts').addClass('active');
+		break;
+		
+	}
+	
+	
+	
+	
+});
