aef191c198ac6b3a74a499f9d46dfc04f0577a81
Rahul <rahulsidhpura912@gmail.com>
2018-06-25 13:59:37 +0530

    61 done as on 25-06-2018

---
+++ os/src/main/java/net/rah/os/controller/GlobalController.java
@@ -0,0 +1,62 @@
+package net.rah.os.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import net.rah.os.model.UserModel;
+import net.rah.osbackend.dao.UserDAO;
+import net.rah.osbackend.dto.User;
+
+@ControllerAdvice
+public class GlobalController {
+
+	@Autowired
+	private HttpSession session;
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	private UserModel userModel=null;
+	
+	@ModelAttribute("userModel")
+	private UserModel getUserModel() {
+		
+		if(session.getAttribute("userModel")==null){
+			
+			//add the user model
+			Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+			User user=userDAO.getByEmail(authentication.getName());
+			
+			if(user!=null){
+				//create a new UserModel object to pass the details
+				userModel=new UserModel();
+				
+				userModel.setId(user.getId());
+				userModel.setEmail(user.getEmail());
+				userModel.setRole(user.getRole());
+				userModel.setFullName(user.getFirstName()+" "+user.getLastName());
+				
+				if(userModel.getRole().equals("USER")){
+					//set the cart only if the user is a buyer
+					userModel.setCart(user.getCart());
+					
+				}
+				
+				//set the userModel in the session
+				session.setAttribute("userModel",userModel);
+				return userModel;
+			}
+			
+		}
+		return (UserModel) session.getAttribute("userModel");
+	}
+	
+	
+	
+	
+}
