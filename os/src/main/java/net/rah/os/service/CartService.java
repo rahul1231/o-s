09bd6eb1c3e0c618437ef975cf6248e26efdda9e
Rahul <rahulsidhpura912@gmail.com>
2018-07-02 14:05:22 +0530

    69 done as on 02-07-2018

--- os/src/main/java/net/rah/os/service/CartService.java
+++ os/src/main/java/net/rah/os/service/CartService.java
@@ -11,6 +11,7 @@ import net.rah.os.model.UserModel;
 import net.rah.osbackend.dao.CartLineDAO;
 import net.rah.osbackend.dto.Cart;
 import net.rah.osbackend.dto.CartLine;
+import net.rah.osbackend.dto.Product;
 
 @Service("cartService")
 public class CartService {
@@ -30,6 +31,42 @@ public class CartService {
 	//returns the entire cart lines
 	public List<CartLine> getCartLines(){
 		return cartLineDAO.list(this.getCart().getId());
+	}
+
+	public String updateCartLine(int cartLineId, int count) {
+		//fetch the cartline
+		CartLine cartLine=cartLineDAO.get(cartLineId);
+		
+		if(cartLine==null){
+			return "result=error";
+		}
+		else{
+			
+			Product product=cartLine.getProduct();
+			
+			double oldTotal=cartLine.getTotal();
+			
+			if(product.getQuantity()<=count){
+				
+				count=product.getQuantity();
+				
+			}
+			
+			cartLine.setProductCount(count);
+			cartLine.setBuyingPrice(product.getUnitPrice());
+			
+			cartLine.setTotal(product.getUnitPrice()*count);
+			
+			cartLineDAO.update(cartLine);
+			
+			Cart cart=this.getCart();
+			
+			cart.setGrandTotal(cart.getGrandTotal()-oldTotal+cartLine.getTotal());
+			
+			cartLineDAO.updateCart(cart);
+			
+			return "result=updated";
+		}
 	}
 	
 }
